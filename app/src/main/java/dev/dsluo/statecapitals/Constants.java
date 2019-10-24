51d161e74a639feb9e098e5e3adcf2460a49bd6c
David Luo <me@dsluo.dev>
2019-10-24 16:26:37 -0400

    static getInstance for database

---
+++ app/src/main/java/dev/dsluo/statecapitals/Constants.java
@@ -0,0 +1,5 @@
+package dev.dsluo.statecapitals;
+
+public class Constants {
+    public static final String DATABASE_NAME = "capitals-db";
+}
--- app/src/main/java/dev/dsluo/statecapitals/database/AppDatabase.java
+++ app/src/main/java/dev/dsluo/statecapitals/database/AppDatabase.java
@@ -1,13 +1,30 @@
 package dev.dsluo.statecapitals.database;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
-@Database(entities = {State.class, Quiz.class}, version = 1)
+import static dev.dsluo.statecapitals.Constants.DATABASE_NAME;
+
+@Database(entities = {State.class, Quiz.class, Question.class}, version = 1)
 @TypeConverters({Converters.class})
 public abstract class AppDatabase extends RoomDatabase {
     public abstract StateDao stateDao();
 
     public abstract QuizDao quizDao();
+
+    private static volatile AppDatabase instance;
+
+    public static AppDatabase getInstance(final Context context) {
+        if (instance == null) {
+            synchronized (AppDatabase.class) {
+                if (instance == null)
+                    instance = Room.databaseBuilder(context, AppDatabase.class, DATABASE_NAME).build();
+            }
+        }
+        return instance;
+    }
 }
