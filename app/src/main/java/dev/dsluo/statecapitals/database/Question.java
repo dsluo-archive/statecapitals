8026941d656cc350d241a8616574a662e3e1c222
David Luo <me@dsluo.dev>
2019-10-26 17:14:25 -0400

    fix warnings about unindexed columns

--- app/src/main/java/dev/dsluo/statecapitals/database/Question.java
+++ app/src/main/java/dev/dsluo/statecapitals/database/Question.java
@@ -3,6 +3,7 @@ package dev.dsluo.statecapitals.database;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
+import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "questions",
@@ -13,7 +14,12 @@ import androidx.room.PrimaryKey;
                 @ForeignKey(entity = State.class,
                         parentColumns = "id",
                         childColumns = "state_id")
-        })
+        },
+        indices = {
+                @Index(value = {"quiz_id", "state_id"}, unique = true),
+                @Index(value = {"state_id"})
+        }
+)
 public class Question {
     @PrimaryKey(autoGenerate = true)
     public int id;
