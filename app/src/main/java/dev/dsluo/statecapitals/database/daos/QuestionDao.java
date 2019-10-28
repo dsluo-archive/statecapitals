c28f5da9600be40cdc27e08e7a33b5f604d8764e
David Luo <me@dsluo.dev>
2019-10-28 08:02:19 -0400

    guarantee order of questions for quiz.

--- app/src/main/java/dev/dsluo/statecapitals/database/daos/QuestionDao.java
+++ app/src/main/java/dev/dsluo/statecapitals/database/daos/QuestionDao.java
@@ -32,6 +32,6 @@ public abstract class QuestionDao extends BaseDao<Question> {
     @Query("SELECT * FROM Question")
     public abstract List<Question> getAll();
 
-    @Query("SELECT * FROM Question WHERE quizId = :quizId")
-    public abstract List<Question> getQuestionsForQuiz(int quizId);
+    @Query("SELECT * FROM Question WHERE quizId = :quizId ORDER BY id ASC")
+    public abstract List<Question> getQuestionsForQuiz(long quizId);
 }
